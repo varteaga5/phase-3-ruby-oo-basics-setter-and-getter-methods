dbc1473ee81502d896029959f7169cbdd19779e9
vincent <vincent.arteaga@gmail.com>
2021-08-05 10:44:57 -0700

    commit tree 7012bb2f6c055245f6314e9be00273c0eee09c0d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    # setter method
+    def name=(name)
+        @name = name
+    end
+    # getter method
+    def name
+        @name
+    end
+    # setter method
+    def breed=(breed)
+        @breed = breed
+    end
+    # getter method
+    def breed
+        @breed
+    end
 
 end
