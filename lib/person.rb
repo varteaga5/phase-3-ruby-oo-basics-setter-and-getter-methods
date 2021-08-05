9893f751ac278df939d7f3c3f200fe3bd2f598fc
vincent <vincent.arteaga@gmail.com>
2021-08-05 10:45:16 -0700

    lab complete

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+    # setter method
+    def name=(name)
+        @name = name
+    end
+    # getter method
+    def name
+        @name
+    end
+    # setter method
+    def job=(job)
+        @job = job
+    end
+    # getter method
+    def job
+        @job
+    end
 
 end
